5b919a06a6e09689281da89d6d4d171005b763e6
nada-elhabashy <nadaelhabashy19@gmail.com>
2022-06-08 02:11:29 +0200

    search

--- comps/SeekerNavbar.js
+++ comps/SeekerNavbar.js
@@ -1,8 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
-import style from "../styles/pages/SeekerHome.module.scss";
+import {  useState, useEffect} from "react";
+import { useRouter } from "next/router";
+const baseUrl = process.env.API_URL + "/jobTitle/search";
 
 const SeekerNavbar = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+ //  const apparels = ["Women", "Kids", "Men"];
+
+
+  const getData = async () => {
+    const res = await fetch(baseUrl);
+    const { data } = await res.json();
+    setJobs(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const searchChangeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //if (document.getElementsByName("search")[0].value !== "")
+      router.push(`/jobTitle/search?jobTitle=${search}`);
+  };
+
   return (
     <ul className="navbar-nav ms-auto">
       <li className="nav-item">
@@ -21,15 +50,18 @@ const SeekerNavbar = () => {
         </Link>
       </li>
       <li className="nav-item searchParent">
+        <form onSubmit={submitHandler} className="searchForm">
         <input
           type="text"
           class="searching"
           placeholder="Search for jobs, companies.."
+          onChange={searchChangeHandler}
         />
 
         <button type="submit">
           <i class="fas fa-search searchIcon"></i>
         </button>
+        </form>
       </li>
       <li className="nav-item">
         <img src="/assets/Landing/logo.png" className="photo" alt="pic"></img>
--- pages/jobTitle/search.js
+++ pages/jobTitle/search.js
@@ -1,6 +1,31 @@
-const Search=()=>{
+import { useRouter } from "next/router";
+import Jobs from "../../comps/jobs";
+const baseUrl = process.env.API_URL;
+
+
+export async function getServerSideProps({ query }) {
+    const res = await fetch(baseUrl + "/jobTitle/search");
+  //  var { postTitle } = await res.json();
+ //   postTitle = postTitle.filter((value) => {
+ //    return value.name !== "";
+//  });
+    const searchQuery = query.search || "";
+    const category = query.category || "";
+  
+    const jobRes = await fetch(
+      baseUrl + "/jobTitle/search?search=" + searchQuery + "&category=" + category
+    );
+    const job = await jobRes.json();
+    return {
+      props: {searchQuery, category, job }, // will be passed to the page component as props
+    };
+  }
+const Search=(props)=>{
+
+    
     return(
         <>
+        <Jobs/>
         </>
     )
 }
