6206cd88889f3b8c8d1f484e18ef1caeac640c7f
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2022-07-07 14:25:20 +0200

    detail pop

--- pages/jobs/[id].js
+++ pages/jobs/[id].js
@@ -3,195 +3,310 @@ import { GoLocation } from "react-icons/go";
 import { SiMaterialdesignicons } from "react-icons/si";
 
 import { useRouter } from "next/router";
-
+import axios from "axios";
 import { useEffect, useContext, useState } from "react";
 
 import Layout from "../../comps/layout";
 import AuthContext from "../../context/AuthContext";
 import style from "../../styles/pages/SeekerHome.module.scss";
 import Spinner from "../../comps/Spinner";
-//import JobDetails from "../../comps/JobDetails";
 
+import SurePop from "../../comps/Popups/SurePop";
+import CVPop from "../../comps/Popups/CVPop";
+import ScreenPop from "../../comps/Popups/ScreenPop";
+import SuccessPop from "../../comps/Popups/SuccessPop";
+import { hideElement } from "../../functions/hideElement";
+import { showElement } from "../../functions/showElement";
+import { ActivateBar } from "../../functions/ActivateBar";
 
 const baseUrl = process.env.API_URL;
 
-
-
-{/*export const  getStaticPaths = async() => 
-{
- 
- const res = await fetch( baseUrl + "/jobs" );
- const data = await res.json();
- 
- const paths =   Object.keys(data).map(details => {
-  
-  return{
-    params: { id: details.id.toString() }
-
-  }
- })
- return{
-  paths,
-  fallback: false
-
- }
-}
- export const getStaticProps = async(context) => {
-  const id = context.params.id;
-  const res = await fetch( baseUrl + "/jobs" +id );
-  const data = await res.json();
-  return{
-    props: {details: data}
-  }
- 
-
- }
-*/}
-
-
 const jobDetails = () => {
-    
-  const { auth } = useContext(AuthContext);
+  const { auth, user } = useContext(AuthContext);
   const router = useRouter();
+
+  //state
   const [detail, setDetail] = useState({});
   const [loading, setLoading] = useState(true);
-  
-  
-  
+  const [cvName, setCVName] = useState("");
+  const [cv_url, setCv_url] = useState("");
+  const [cvError, setCVError] = useState("");
+  const [screenError, setScreenError] = useState("");
+  //
+  const [text_questions, setText_questions] = useState([]);
+  const [text_answers, setText_answers] = useState([]);
+  const [application_id, setApplication_id] = useState("");
+
   const getDetails = async () => {
-  
-   const path =  router.asPath.substring(1);
-  
+    const path = router.asPath.substring(1);
+
     const res = await fetch(baseUrl + path, {
       headers: {
         Authorization: "Bearer" + " " + auth,
       },
     });
-    const {msg} = await res.json();
-  console.log(msg);
-  
-  
+    const { msg } = await res.json();
+
     setDetail(msg);
-  
+    // setText_questions(msg.application.text_questions);
+
     setLoading(false);
   };
   useEffect(() => {
     getDetails();
   }, [router]);
 
- 
- 
-  return (
-    loading ? (
-      <Spinner />
-    ) : (
-   <>
- {/*  <JobDetails details={ids}/>*/}
+  //handlers;
+  const disableBtn = (id) => {
+    const btn = document.getElementById(id);
+    btn.style.background = "#9C93F8";
+    btn.style.cursor = "auto";
+    btn.disabled = true;
+  };
 
-         <div className={style.box}>
-           <span className={style.right}>
-             <div>
-               
-   
-             </div>
-           
-               <div className={style.boxDetails} >
-                 <div className={style.jobImgContainer}>
-                   <img
-                     className={style.jobImg}
-                     alt="hey"
-                     src="/assets/Landing/logo.png"
-                   />
-                 </div>
-   
-                 <div className={style.informations}>
-                  
-                     <div className={style.postTitle}> {detail.title} </div>
-                   
-                     <div className={style.postTitle}> {detail["job title"]} </div>
-                  
-   
-                   
-                     <div className={style.job_type}> {detail.job_type} </div>
-                   
-   
-                  
-                     <div className={style.subInfo}>
-                       <div className={style.experience}>
-                         {" "}
-                         <i className={style.expIcon}>
-                           <SiMaterialdesignicons />
-                         </i>
-                         {detail.experience}{" "}
-                       </div>
-                       <div className={style.location}>
-                         {" "}
-                         <i className={style.locationIcon}>
-                           <GoLocation />
-                         </i>
-                         {detail.location}{" "}
-                       </div>
-                     </div>
-                   
-                 </div>
-                
-                 <div >
-                 <button
-                     className={` btn--global btn--detail btn--save ${style.btnSave}`}
-                     type="submit"
-                   >
-                     Save
-                   </button>
-                
-                 <button
-                     className={` btn--global btn--detail btn--blue ${style.btnDetails}`}
-                     type="submit"
-                   >
-                    Apply
-                   </button>
-                   </div>
-              
-               </div>
-              
-               <div className={style.boxDetails} >
-               <div className="">
-               <div>
-               <h3 className="circlebef"> Job Discription</h3> 
-               </div>
-          </div>
-          
-          <pre>{detail.description }</pre>
-          
-               </div>
-               
-               <div className={style.boxDetails} >
-               <div className="">
-               <div>
-               <h3 className="circlebef"> Job Requirments</h3> 
-               </div>
-          </div>
-          <div>
-          <pre>{detail.requirements }</pre>
-          </div>
-               </div>
-           </span>
-         </div>
+  const EnableBtn = (id) => {
+    const btn = document.getElementById(id);
+    btn.style.background = "#5c46f9";
+    btn.style.cursor = "pointer";
+    btn.disabled = false;
+  };
+  const ApplyHandler = async (e, btn_id, id) => {
+    e.preventDefault();
+    disableBtn(btn_id);
+    const user_id = user.id;
 
-      
-
-    
-    </>  )
-    
-      );
-
+    // waiting for api response
+    const res = await fetch(baseUrl + "seeker/details/view/" + user_id);
+    const { CV } = await res.json();
+    if (CV) {
+      setCVName(CV.fileName.split("$")[0] + ".pdf");
+      setCv_url(CV.fileUrl);
     }
+    hideElement(id);
+    EnableBtn(btn_id);
+    showElement("CVpop");
+  };
 
+  const CVHandler = async (e, btn_id, id) => {
+    e.preventDefault();
+    if (!cv_url) {
+      setCVError("An updated Resume must be included!");
+      return;
+    }
+    setCVError("");
+    disableBtn(btn_id);
+
+    // waiting for api response - will be removed in job details
+    //
+    const res = await fetch(baseUrl + "jobs/62a8bc6bd20159963aec17ca", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer" + " " + auth,
+      },
+    });
+    const { msg } = await res.json();
+    setText_questions(msg.application.text_questions);
+    //
+    hideElement(id);
+    EnableBtn(btn_id);
+    showElement("ScreenPop");
+  };
+
+  const ScreenHandler = async (e, btn_id, id) => {
+    e.preventDefault();
+    if (text_answers.length === 0) {
+      setScreenError("Please answer the required questions!");
+      return;
+    }
+    disableBtn(btn_id);
+    //waiting for api response
+    let response = await fetch(baseUrl + `applications/${detail._id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer" + " " + auth,
+      },
+      body: JSON.stringify({
+        text_question: text_questions,
+        text_answers,
+        cv_url,
+      }),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          setScreenError("");
+          const res = await response.json();
+          const { msg } = await res;
+          setApplication_id(msg._id);
+          EnableBtn(btn_id);
+          hideElement(id);
+          showElement("successPop");
+        } else {
+          const res = await response.json();
+          const { msg } = await res;
+          setScreenError(msg);
+          EnableBtn(btn_id);
+          throw new Error(msg);
+        }
+      })
+
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  // submittion
+  const uploadCV = ({ target: { files } }) => {
+    const extension = files[0].name.split(".")[1];
+    if (extension !== "pdf") {
+      setCVError("Only pdf files are allowed!");
+      return;
+    }
+    disableBtn("cvl");
+    let data = new FormData();
+    data.append("file", files[0]);
+    const options = {
+      headers: {
+        Authorization: "Bearer" + " " + auth,
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    axios
+      .post(baseUrl + "upload-file", data, options)
+      .then((res) => {
+        setCVError("");
+        const { data } = res;
+        setCVName(data.msg.original_name);
+        setCv_url(data.msg.file_url);
+        EnableBtn("cvl");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <div className={style.box}>
+        <span className={style.right}>
+          <div></div>
+
+          <div className={style.boxDetails}>
+            <div className={style.jobImgContainer}>
+              <img
+                className={style.jobImg}
+                alt="hey"
+                src="/assets/Landing/logo.png"
+              />
+            </div>
+
+            <div className={style.informations}>
+              <div className={style.postTitle}> {detail.title} </div>
+
+              <div className={style.postTitle}> {detail["job title"]} </div>
+
+              <div className={style.job_type}> {detail.job_type} </div>
+
+              <div className={style.subInfo}>
+                <div className={style.experience}>
+                  {" "}
+                  <i className={style.expIcon}>
+                    <SiMaterialdesignicons />
+                  </i>
+                  {detail.experience}{" "}
+                </div>
+                <div className={style.location}>
+                  {" "}
+                  <i className={style.locationIcon}>
+                    <GoLocation />
+                  </i>
+                  {detail.location}{" "}
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <button
+                className={` btn--global btn--detail btn--save ${style.btnSave}`}
+                type="submit"
+              >
+                Save
+              </button>
+
+              <button
+                className={` btn--global btn--detail btn--blue ${style.btnDetails}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  showElement("sure");
+                }}
+              >
+                Apply
+              </button>
+            </div>
+          </div>
+
+          <div className={style.boxDetails}>
+            <div className="">
+              <div>
+                <h3 className="circlebef"> Job Discription</h3>
+              </div>
+            </div>
+
+            <pre>{detail.description}</pre>
+          </div>
+
+          <div className={style.boxDetails}>
+            <div className="">
+              <div>
+                <h3 className="circlebef"> Job Requirments</h3>
+              </div>
+            </div>
+            <div>
+              <pre>{detail.requirements}</pre>
+            </div>
+          </div>
+        </span>
+      </div>
+      {/* job details popups */}
+      <SurePop
+        header="Apply for job"
+        content="Are you sure you want to apply to this job?"
+        submit="Apply"
+        handler={ApplyHandler}
+      />
+      <CVPop
+        header={`Apply To ${detail.title}`}
+        name={cvName}
+        submit="Next"
+        link={cv_url}
+        uploadCV={uploadCV}
+        handler={CVHandler}
+        error={cvError}
+      />
+      <ScreenPop
+        header={`Apply To ${detail.title}`}
+        text_questions={text_questions}
+        text_answers={text_answers}
+        setText_answers={setText_answers}
+        handler={ScreenHandler}
+        error={screenError}
+      />
+      <SuccessPop
+        content={`Your application was sent to ${detail.title}!`}
+        submit="Track Application"
+        application_id={application_id}
+      />
+    </>
+  );
+};
 
 jobDetails.getLayout = function getLayout(page) {
-    return <Layout title="Job Details">{page}</Layout>;
-  };
+  return <Layout title="Job Details">{page}</Layout>;
+};
 export default jobDetails;
-
 
 // arr = ["women", "kids", "men"]
 
@@ -199,7 +314,6 @@ export default jobDetails;
 // 0
 // 1
 // 2
-
 
 // [html, css, js]
 
--- src/SeekerHome.js
+++ src/SeekerHome.js
@@ -3,34 +3,15 @@ import { useContext, useEffect, useState } from "react";
 import Spinner from "../comps/Spinner";
 import Jobs from "../comps/jobs";
 
-// job details import
-import SurePop from "../comps/Popups/SurePop";
-import CVPop from "../comps/Popups/CVPop";
-import { hideElement } from "../functions/hideElement";
-import { showElement } from "../functions/showElement";
-import axios from "axios";
-import ScreenPop from "../comps/Popups/ScreenPop";
-import SuccessPop from "../comps/Popups/SuccessPop";
-import { ActivateBar } from "../functions/ActivateBar";
-
 const baseUrl = process.env.API_URL;
 
 const SeekerHome = () => {
   //hooks
-  const { auth, user } = useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
 
   // state
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
-
-  // job details state
-  const [cvName, setCVName] = useState("");
-  const [cv_url, setCv_url] = useState("");
-  const [cvError, setCVError] = useState("");
-  const [screenError, setScreenError] = useState("");
-  const [text_questions, setText_questions] = useState([]);
-  const [text_answers, setText_answers] = useState([]);
-  const [application_id, setApplication_id] = useState("");
 
   // call backend to retrieve jobs
   const getData = async () => {
@@ -52,135 +33,6 @@ const SeekerHome = () => {
     ActivateBar("bar1");
   }, []);
 
-  //  job details handlers
-  const disableBtn = (id) => {
-    const btn = document.getElementById(id);
-    btn.style.background = "#9C93F8";
-    btn.style.cursor = "auto";
-    btn.disabled = true;
-  };
-
-  const EnableBtn = (id) => {
-    const btn = document.getElementById(id);
-    btn.style.background = "#5c46f9";
-    btn.style.cursor = "pointer";
-    btn.disabled = false;
-  };
-  const ApplyHandler = async (e, btn_id, id) => {
-    e.preventDefault();
-    disableBtn(btn_id);
-    const user_id = user.id;
-
-    // waiting for api response
-    const res = await fetch(baseUrl + "seeker/details/view/" + user_id);
-    const { CV } = await res.json();
-    if (CV) {
-      setCVName(CV.fileName.split("$")[0] + ".pdf");
-      setCv_url(CV.fileUrl);
-    }
-    hideElement(id);
-    EnableBtn(btn_id);
-    showElement("CVpop");
-  };
-
-  const CVHandler = async (e, btn_id, id) => {
-    e.preventDefault();
-    if (!cv_url) {
-      setCVError("An updated Resume must be included!");
-      return;
-    }
-    setCVError("");
-    disableBtn(btn_id);
-
-    // waiting for api response - will be removed in job details
-    const res = await fetch(baseUrl + "jobs/62a8bc6bd20159963aec17ca", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer" + " " + auth,
-      },
-    });
-    const { msg } = await res.json();
-    setText_questions(msg.application.text_questions);
-    hideElement(id);
-    EnableBtn(btn_id);
-    showElement("ScreenPop");
-  };
-
-  const ScreenHandler = async (e, btn_id, id) => {
-    e.preventDefault();
-    if (text_answers.length === 0) {
-      setScreenError("Please answer the required questions!");
-      return;
-    }
-    disableBtn(btn_id);
-    //waiting for api response
-    let response = await fetch(
-      baseUrl + "applications/62a8bc6bd20159963aec17ca",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer" + " " + auth,
-        },
-        body: JSON.stringify({
-          text_question: text_questions,
-          text_answers,
-          cv_url,
-        }),
-      }
-    )
-      .then(async (response) => {
-        if (response.ok) {
-          setScreenError("");
-          const res = await response.json();
-          const { msg } = await res;
-          setApplication_id(msg._id);
-          EnableBtn(btn_id);
-          hideElement(id);
-          showElement("successPop");
-        } else {
-          const res = await response.json();
-          const { msg } = await res;
-          setScreenError(msg);
-          EnableBtn(btn_id);
-          throw new Error(msg);
-        }
-      })
-
-      .catch((e) => {
-        console.log(e);
-      });
-  };
-  //  job details submittion
-  const uploadCV = ({ target: { files } }) => {
-    const extension = files[0].name.split(".")[1];
-    if (extension !== "pdf") {
-      setCVError("Only pdf files are allowed!");
-      return;
-    }
-    disableBtn("cvl");
-    let data = new FormData();
-    data.append("file", files[0]);
-    const options = {
-      headers: {
-        Authorization: "Bearer" + " " + auth,
-        "Content-Type": "multipart/form-data",
-      },
-    };
-
-    axios
-      .post(baseUrl + "upload-file", data, options)
-      .then((res) => {
-        setCVError("");
-        const { data } = res;
-        setCVName(data.msg.original_name);
-        setCv_url(data.msg.file_url);
-        EnableBtn("cvl");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
   return (
     <>
       {loading ? (
@@ -188,35 +40,6 @@ const SeekerHome = () => {
       ) : (
         <>
           <Jobs posts={posts} />
-          {/* job details popups */}
-          <SurePop
-            header="Apply for job"
-            content="Are you sure you want to apply to this job?"
-            submit="Apply"
-            handler={ApplyHandler}
-          />
-          <CVPop
-            header="Apply To Litfair"
-            name={cvName}
-            submit="Next"
-            link={cv_url}
-            uploadCV={uploadCV}
-            handler={CVHandler}
-            error={cvError}
-          />
-          <ScreenPop
-            header="Apply To Litfair"
-            text_questions={text_questions}
-            text_answers={text_answers}
-            setText_answers={setText_answers}
-            handler={ScreenHandler}
-            error={screenError}
-          />
-          <SuccessPop
-            content="Your application was sent to LitFair!"
-            submit="Track Application"
-            application_id={application_id}
-          />
         </>
       )}
     </>
