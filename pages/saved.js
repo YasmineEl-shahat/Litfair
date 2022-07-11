5763abd541e607b6f29441cd0e2eca04ed1eeede
nada-elhabashy <nadaelhabashy19@gmail.com>
2022-07-11 20:19:02 +0200

    saved

--- pages/saved.js
+++ pages/saved.js
@@ -1,18 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect,useContext, useState } from "react";
 import Spinner from "../comps/Spinner";
 import Jobs from "../comps/jobs";
 import { ActivateBar } from "../functions/ActivateBar";
 import Layout from "../comps/layout";
+import AuthContext from "../context/AuthContext";
+
+
+
+const baseUrl = process.env.API_URL;
 
 const Saved = () => {
   // state
   const [loading, setLoading] = useState(true);
   const [savedJobs, setSavedJobs] = useState([]);
+  const { auth } = useContext(AuthContext);
+
+  // const getData = async () => {
+  //   const saved = localStorage.getItem("saved")
+  //     ? JSON.parse(localStorage.getItem("saved"))
+  //     : [];
+  //   setSavedJobs(saved);
+  //   setLoading(false);
+  // };
+  
   const getData = async () => {
-    const saved = localStorage.getItem("saved")
-      ? JSON.parse(localStorage.getItem("saved"))
-      : [];
-    setSavedJobs(saved);
+    
+    const res = await fetch(baseUrl + "seeker/saved-jobs"
+    , {
+      headers: {
+        Authorization: "Bearer" + " " + auth,
+      },
+    }
+    );
+    const { msg } = await res.json();
+
+   console.log(msg);
+    setSavedJobs(msg);
+
+
     setLoading(false);
   };
 
