43ea3346da0da48675369cbf2ab954dc9b5f39c2
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2022-07-12 20:08:06 +0200

    empty dashboard

--- src/CompanyHome.js
+++ src/CompanyHome.js
@@ -1,20 +1,28 @@
-import style from "../styles/pages/CompanyHome.module.scss";
+import style from "../styles/pages/Company.module.scss";
 import Link from "next/link";
+import Empty from "../comps/empty";
 const CompanyHome = () => {
   return (
-    <main className="container">
-      <section className={style.header}>
+    <main className={`container `}>
+      <div className={style.header}>
         <h4>Hello!</h4>
         <Link href="/post-job" passHref>
           <button className="btn--global btn--blue btn--detail">
             Add New Post
           </button>
         </Link>
-      </section>
-      <section className="row">
-        <div className={`${style.content} col-md-8`}>g</div>
-        <div className={`${style.content} col-md-4`}>g</div>
-      </section>
+      </div>
+      <div className={` ${style.ContentWrap}`}>
+        <section className={`${style.content} `}>
+          <Empty
+            txt1="There is no posted jobs for now!"
+            btn="Add New Post"
+            path="/post-job"
+            isCompany={true}
+          />
+        </section>
+        <section className={`${style.content} `}></section>
+      </div>
     </main>
   );
 };
