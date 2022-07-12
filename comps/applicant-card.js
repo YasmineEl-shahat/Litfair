97a942e6295e2021572149a677cdb37d8e25b99d
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2022-07-12 05:37:09 +0200

    top applicants

--- comps/applicant-card.js
+++ comps/applicant-card.js
@@ -1,19 +1,50 @@
 import Link from "next/link";
+import { disableBtn, EnableBtn } from "../functions/ButtonsFun";
 import { showElement } from "../functions/showElement";
 
 export const Card = ({
+  applicant,
   imgSrc,
   name,
   rate,
   feedback,
   setFeedbackPop,
   setNamePop,
+  select,
+  setSelect,
 }) => {
+  // variables
+  const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       <div className="Card">
-        <img src={imgSrc} className="photo" alt="pic"></img>
+        <div
+          onClick={() => {
+            let newSelect = select;
+            if (select.includes(applicant)) {
+              newSelect = newSelect.filter((sel) => sel !== applicant);
+            } else {
+              newSelect = [...newSelect, applicant];
+            }
+            setSelect(newSelect);
+            if (newSelect.length) EnableBtn("applybtn");
+            else disableBtn("applybtn");
+          }}
+          className="selectbtn"
+        >
+          {select.includes(applicant) && <i class="fa-solid fa-check"></i>}
+        </div>
+        <img src={imgSrc} className="card-photo" alt="pic"></img>
         <h4>{name}</h4>
+        <section className="starWrap">
+          {stars.map((star) =>
+            star <= Math.floor(rate) ? (
+              <img key={star} alt="rate" src="/assets/company/star.png" />
+            ) : (
+              <img key={star} alt="rate" src="/assets/company/dark star.png" />
+            )
+          )}
+        </section>
         <p>Rate:{rate}</p>
 
         <button
@@ -23,7 +54,6 @@ export const Card = ({
             showElement("FeedPop");
           }}
           className={`btn--global btn--detail btn--blue`}
-          type="submit"
         >
           Show Interview Result
         </button>
--- pages/companies/jobs/[id].js
+++ pages/companies/jobs/[id].js
@@ -5,14 +5,22 @@ import { Card } from "../../../comps/applicant-card";
 import { useEffect, useContext, useState } from "react";
 import AuthContext from "../../../context/AuthContext";
 import FeedPop from "../../../comps/Popups/FeedPop";
+import SurePop from "../../../comps/Popups/SurePop";
+import { showElement } from "../../../functions/showElement";
+import { EnableBtn, disableBtn } from "../../../functions/ButtonsFun";
+import { GoLocation } from "react-icons/go";
 
 const baseUrl = process.env.API_URL;
 
 const TopApplicants = () => {
   //state
   const [applicants, setApplicants] = useState([]);
+  const [job_title, setJob_title] = useState({});
+  const [job_type, setJob_type] = useState({});
+  const [job_location, setJob_location] = useState({});
   const [feedbackPop, setFeedbackPop] = useState({});
   const [namePop, setNamePop] = useState("");
+  const [select, setSelect] = useState([]);
   // hooks
   const { auth } = useContext(AuthContext);
   const getDetails = async () => {
@@ -23,10 +31,15 @@ const TopApplicants = () => {
       },
     });
     const { msg } = await res.json();
-    setApplicants(msg);
+    setApplicants(msg.applications);
+    setJob_title(msg.job_title);
+    setJob_type(msg.job_type);
+    setJob_location(msg.job_location);
   };
   useEffect(async () => {
     getDetails();
+
+    disableBtn("applybtn");
   }, []);
 
   return (
@@ -37,10 +50,49 @@ const TopApplicants = () => {
       >
         {" "}
         <article className={styleVid.questionCarCont}>
+          {/* top header section */}
+          <section className={style.Top}>
+            <h2>{job_title}</h2>
+            <div>
+              <h6>
+                <GoLocation /> {job_location}
+              </h6>
+              <h6>
+                <i class="fa-regular fa-clock"></i> {job_type}
+              </h6>
+            </div>
+          </section>
+          {/* end top header section */}
+
+          <section className={style.Head}>
+            <h4>TOP {applicants.length} APPLICANTS</h4>
+            <h5>Select who will do an interview with you!</h5>
+            <article>
+              <div className={style.selectAll}>
+                <h5>select all</h5>{" "}
+                <div
+                  onClick={(e) => {
+                    if (select === applicants) {
+                      setSelect([]);
+                      e.target.classList.remove("fa-check");
+                      disableBtn("applybtn");
+                    } else {
+                      setSelect(applicants);
+                      e.target.classList.add("fa-check");
+                      EnableBtn("applybtn");
+                    }
+                  }}
+                  className="selectbtn fa-solid "
+                ></div>
+              </div>
+            </article>
+          </section>
+          {/* cards wrapper section */}
           <section className={style.cardWrap}>
             {applicants.map((applicant) => (
               <div key={applicant._id}>
                 <Card
+                  applicant={applicant}
                   imgSrc="/assets/profile/blank-profile-picture.png"
                   name={applicant.fname + " " + applicant.lname}
                   rate={applicant.feedback_1.total_score}
@@ -48,13 +100,32 @@ const TopApplicants = () => {
                   feedback={applicant.feedback_1}
                   setFeedbackPop={setFeedbackPop}
                   setNamePop={setNamePop}
+                  select={select}
+                  setSelect={setSelect}
                 />
               </div>
             ))}
           </section>
+          {/* end cards wrapper section */}
+          <section className={style.Head}>
+            <button
+              id="applybtn"
+              onClick={() => {
+                showElement("sure");
+              }}
+              className={`btn--global btn--detail btn--blue ${style.apply}`}
+            >
+              Apply
+            </button>
+          </section>
         </article>
       </main>
       <FeedPop feedback={feedbackPop} name={namePop} />
+      <SurePop
+        submit="let's go"
+        header={`${select.length} job seekers was selected`}
+        content="let's schedule your interview hours"
+      />
     </>
   );
 };
