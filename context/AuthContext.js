8f0f1b18baf5e07ed05379e60fb53214c5f4c0b0
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2022-07-12 16:40:02 +0200

    refed

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -61,7 +61,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const path = ["/login", "/seekerRegister", "/companyRegister"];
     if (!path.includes(router.asPath) && auth) checkToken();
-    else if (!path.includes(router.asPath) && auth) router.replace("/");
+    else if (!path.includes(router.asPath)) router.replace("/");
     let hour = 1000 * 60 * 60;
     let interval = setInterval(function () {
       if (!path.includes(router.asPath) && auth) checkToken();
